46a83c0a84cfb8f5ace2c4c1c00d355853f95265
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-01-30 16:11:39 +0100

    entidades
    uso

--- src/main/java/com/example/r3/controller/R3controller.java
+++ src/main/java/com/example/r3/controller/R3controller.java
@@ -1,4 +1,9 @@
 package com.example.r3.controller;
 
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@NoArgsConstructor
+@Controller
 public class R3controller {
 }
--- src/main/java/com/example/r3/model/entities/User.java
+++ src/main/java/com/example/r3/model/entities/User.java
@@ -1,4 +1,13 @@
 package com.example.r3.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    String username;
+
 }
