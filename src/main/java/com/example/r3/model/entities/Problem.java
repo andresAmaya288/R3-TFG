f501fbb01ac18be2486a77d411ab1ab30b4f924c
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-01-30 22:02:43 +0100

    update problem

--- src/main/java/com/example/r3/model/entities/Problem.java
+++ src/main/java/com/example/r3/model/entities/Problem.java
@@ -1,5 +1,6 @@
 package com.example.r3.model.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.SecureRandom;
 
 
 @NoArgsConstructor
@@ -19,8 +22,6 @@ public class Problem {
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10, new SecureRandom());
 
     @Column(nullable = true)
-    @JsonView(Token.Basic.class)
-    @JsonIgnore
     private String hashedPassword;
 
 }
