65a63a852b96d108f8edf5e6a5780982e59a9551
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-01-30 16:38:44 +0100

    encoder

--- src/main/java/com/example/r3/model/entities/Problem.java
+++ src/main/java/com/example/r3/model/entities/Problem.java
@@ -2,7 +2,11 @@ package com.example.r3.model.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @NoArgsConstructor
@@ -10,4 +14,13 @@ import lombok.NoArgsConstructor;
 public class Problem {
     @Id
     int id;
+    @Transient
+    @Autowired
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10, new SecureRandom());
+
+    @Column(nullable = true)
+    @JsonView(Token.Basic.class)
+    @JsonIgnore
+    private String hashedPassword;
+
 }
