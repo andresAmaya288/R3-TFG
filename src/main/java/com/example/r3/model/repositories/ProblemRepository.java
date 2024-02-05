4fcbfee82aa9b11ebc12cb87ecf81333469252cf
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-02-05 20:52:57 +0100

    repository

--- src/main/java/com/example/r3/model/repositories/ProblemRepository.java
+++ src/main/java/com/example/r3/model/repositories/ProblemRepository.java
@@ -1,4 +1,6 @@
 package com.example.r3.model.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.r3.model.entities.Problem;
 
-public class ProblemRepository {
+public interface ProblemRepository extends JpaRepository <Problem, Long>{
 }
--- src/main/java/com/example/r3/model/repositories/UserRepository.java
+++ src/main/java/com/example/r3/model/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.r3.model.repositories;
 
-public class UserRepository {
+import com.example.r3.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findByUserName(String name);
 }
