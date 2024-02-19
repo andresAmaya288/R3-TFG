6a8c3b6ebcbedd39387dc3fb988c149e4d24bb93
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-02-19 18:17:39 +0100

    html

--- src/main/java/com/example/r3/model/services/ProblemService.java
+++ src/main/java/com/example/r3/model/services/ProblemService.java
@@ -1,4 +1,10 @@
 package com.example.r3.model.services;
 
+import com.example.r3.model.entities.Problem;
+
 public class ProblemService {
+
+    public Problem addProblem (Problem problem){
+        
+    }
 }
