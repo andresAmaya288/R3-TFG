47a42ac5e18c3c155d07b86ca2139e06d9ca9af1
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2024-04-14 14:10:17 +0200

    diagrama funcionaaaa!!!!

--- src/main/resources/static/js/diagram.js
+++ src/main/resources/static/js/diagram.js
@@ -2,47 +2,21 @@ function getSolution(source,destiny) {
 
     var initialValue = document.getElementById(source).value;
     var url = document.getElementById("apiSolUrl").value;
+
+    if(initialValue == null){
+        initialValue = document.getElementById(source).textContent;
+    }
+
     // Create an object with the data to send
     var data = initialValue;
 
     // Create the HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-
-    // Set up the response callback
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                document.getElementById(destiny).textContent = xhr.responseText;
-            } else {
-                console.log('Request error. Status code:', xhr.status);
-            }
-        }
-    };
-
-    // Send the request with the form data in JSON format
-    xhr.send(encodeURIComponent(data));
-}
-
-function getSubproblem(source, code, destiny, final) {
-
-    var downCode = document.getElementById(code).value;
-    var input = document.getElementById(source).value;
-    var url = document.getElementById("apiSubUrl").value;
-
-    var data = {
-        input: input,
-        downCode:downCode
-    };
-
-
     var options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body:data
     };
 
     fetch(url, options)
@@ -55,21 +29,83 @@ function getSubproblem(source, code, destiny, final) {
         })
         .then(function(data) {
             console.log('Response body:', data);
-            document.getElementById('subproblem').textContent = data;
+            document.getElementById(destiny).textContent = data;
 
         })
         .catch(function(error) {
             console.error('There was a problem with the fetch request:', error.message);
         });
 
-    getSolution(destiny,final);
+}
 
+function getSubproblem(source, code, destiny, destiny2) {
+    var downCode = document.getElementById(code).value;
+    var input = document.getElementById(source).value;
+    var url = document.getElementById("apiSubUrl").value;
+
+    var data = {
+        input: input,
+        downCode: downCode
+    };
+
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch(url, options)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Error submitting form. Response status: ' + response.status);
+            }
+        })
+        .then(function (data) {
+            console.log('Response body:', data);
+            document.getElementById(destiny).textContent = data;
+
+            var url2 = document.getElementById("apiSolUrl").value;
+            var initialValue = document.getElementById(destiny).textContent;
+
+            var data2 = initialValue;
+
+            var options2 = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: data2
+            };
+
+            fetch(url2, options2)
+                .then(function (response2) {
+                    if (response2.ok) {
+                        return response2.json();
+                    } else {
+                        throw new Error('Error submitting form. Response status: ' + response2.status);
+                    }
+                })
+                .then(function (data2) {
+                    console.log('Response body:', data2);
+                    document.getElementById(destiny2).textContent = data2;
+                })
+                .catch(function (error) {
+                    console.error('There was a problem with the fetch request:', error.message);
+                });
+        })
+        .catch(function (error) {
+            console.error('There was a problem with the fetch request:', error.message);
+        });
 }
 
 
 document.getElementById("input").addEventListener("change", function() {
     getSolution('input','solution');
-    getSubproblem('input', 'downCode', 'subproblem','subsolution');
+    getSubproblem('input', 'downCode','subproblem','subsolution');
 });
 
 document.getElementById("downCode").addEventListener("change", function() {
